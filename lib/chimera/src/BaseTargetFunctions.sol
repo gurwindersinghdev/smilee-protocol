fa0b252a56ec12304a5ba2bcbe77cdab408a1e78
g <gurwindersingh00064@gmail.com>
2024-04-03 11:18:32 +0530

    changes

--- lib/chimera/src/BaseTargetFunctions.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "./Setup.sol";
-import "./Asserts.sol";
-
-abstract contract BaseTargetFunctions is Setup, Asserts {}
--- lib/chimera/src/Setup.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-abstract contract Setup {
-    function setup() internal virtual;
-}
